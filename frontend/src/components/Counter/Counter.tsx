e81036062e66afc1ae278deee52be568fed4d74b
bogdy234 <filimonbogdan89@gmail.com>
2022-11-15 16:33:50 +0200

    remove style from counter

--- frontend/src/components/Counter/Counter.tsx
+++ frontend/src/components/Counter/Counter.tsx
@@ -1,11 +1,9 @@
 import Button from "@mui/material/Button";
 import { FC, useState } from "react";
-import useStyles from "./style";
 
 interface CounterProps {}
 
 const Counter: FC<CounterProps> = () => {
-    const classNames = useStyles();
     const [count, setCount] = useState<number>(0);
 
     return (
@@ -13,7 +11,6 @@ const Counter: FC<CounterProps> = () => {
             variant="contained"
             onClick={() => setCount(count + 1)}
             size="large"
-            className={classNames.text_alb}
         >
             {count}
         </Button>
--- frontend/src/components/Counter/style.ts
+++
@@ -1,9 +0,0 @@
-import { makeStyles } from "@mui/material";
-
-const useStyles = makeStyles(() => ({
-    text_alb: {
-        color: "white",
-    },
-}));
-
-export default useStyles;
