341829fd236210a71cdf10a457d1002d3846b41e
bogdy234 <filimonbogdan89@gmail.com>
2022-11-15 16:16:25 +0200

    wip

--- frontend/src/components/Counter/Counter.tsx
+++ frontend/src/components/Counter/Counter.tsx
@@ -1,9 +1,11 @@
 import Button from "@mui/material/Button";
 import { FC, useState } from "react";
+import useStyles from "./style";
 
 interface CounterProps {}
 
 const Counter: FC<CounterProps> = () => {
+    const classNames = useStyles();
     const [count, setCount] = useState<number>(0);
 
     return (
@@ -11,6 +13,7 @@ const Counter: FC<CounterProps> = () => {
             variant="contained"
             onClick={() => setCount(count + 1)}
             size="large"
+            className={classNames.text_alb}
         >
             {count}
         </Button>
---
+++ frontend/src/components/Counter/style.ts
@@ -0,0 +1,9 @@
+import { makeStyles } from "@mui/material";
+
+const useStyles = makeStyles(() => ({
+    text_alb: {
+        color: "white",
+    },
+}));
+
+export default useStyles;
