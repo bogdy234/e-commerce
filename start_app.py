19f8f9f8683816f30863226f936728ad028f162b
Sebastian Mihalache <sebastian.mihalache@devnest.ro>
2022-11-15 19:14:33 +0200

    Now you can build the project by parts

--- start_app.py
+++ start_app.py
@@ -7,7 +7,10 @@ def show_help():
 
     How to use: python start_app.py [-build|-start|-stop|-restart|-up]"
     Options:
-    -build    Build the images on docker
+    -build    Build all the images on docker
+    -build_backend  Build only backend image
+    -build_frontend Build only frontend image
+    -build_db   Build only db image
     -start    Start the image on docker
     -stop     Stop the image on docker
     -restart    Restart the image on docker
@@ -16,6 +19,15 @@ def show_help():
 
 def build_project():
     os.system("cd backend/ && docker-compose build")
+
+def build_project_backend():
+    os.system("cd backend/ && docker-compose build app")
+
+def build_project_frontend():
+    os.system("cd backend/ && docker-compose build frontend")
+
+def build_project_db():
+    os.system("cd backend/ && docker-compose build db")
 
 def start_project():
     os.system("cd backend/ && docker-compose start")
@@ -31,6 +43,9 @@ def up_project():
     
 parser = argparse.ArgumentParser(description="Put the argument")
 parser.add_argument("-build","-build",const="build", action="store_const",default=None)
+parser.add_argument("-build_backend","-build_backend",const="build", action="store_const",default=None)
+parser.add_argument("-build_frontend","-build_frontend",const="build", action="store_const",default=None)
+parser.add_argument("-build_db","-build_db",const="build", action="store_const",default=None)
 parser.add_argument("-start","-start",const="start", action="store_const",default=None)
 parser.add_argument("-stop","-stop",const="stop", action="store_const",default=None)
 parser.add_argument("-restart","-restart",const="restart", action="store_const",default=None)
@@ -39,6 +54,9 @@ parser.add_argument("-help","-help",const="show_help", action="store_const",defa
 
 args = parser.parse_args()
 build = args.build
+build_backend = args.build_backend
+build_frontend = args.build_frontend
+build_db = args.build_db
 start = args.start
 stop = args.stop
 restart = args.restart
@@ -49,6 +67,12 @@ if helper:
     show_help()
 if build:
     build_project()
+if build_backend:
+    build_project_backend()
+if build_frontend:
+    build_project_frontend()
+if build_db:
+    build_project_db()
 if start:
     start_project()
 if stop:
