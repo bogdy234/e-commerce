f1c6ed3485a7f9f09dde394893b11f2bdd0ce3b6
Sebastian Mihalache <sebastian.mihalache@devnest.ro>
2022-11-20 21:56:20 +0200

    Secure = false if not production

--- backend/view/users/login_view.py
+++ backend/view/users/login_view.py
@@ -1,6 +1,6 @@
 from flask import jsonify, request, make_response
 
-from application import app
+from application import app, production
 from controller.users.LoginController import LoginController
 
 
@@ -12,5 +12,5 @@ def login():
         permanent_session = request.json["remember"]
         data_login = LoginController().login(email, password, permanent_session)
         resp = make_response(jsonify(data_login), data_login.get("code", 500))
-        resp.set_cookie("jwt",data_login.get("token"),secure=True,httponly=True,samesite=None)
+        resp.set_cookie("jwt",data_login.get("token"),secure=False if not production else True,httponly=True,samesite=None)
         return resp
